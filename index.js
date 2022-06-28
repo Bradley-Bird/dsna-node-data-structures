14ebba1f82e90d4bc79704fbe124f40fadf8cb09
Anddy <alonzoa2015@gmail.com>
2022-06-28 10:30:25 -0700

    BinaryTreeNode working?

--- index.js
+++ index.js
@@ -46,4 +46,57 @@ console.log(list.getList());
 list.removeTail();
 console.log(list.getList());
 
-module.exports = { LinkedList };
+
+class BinaryTreeNode {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  
+    add(node) {
+        if (node.value === this.value) {
+            if (!this.left) {
+              this.left = node;
+            } else {
+              this.left.add(node);
+            }
+          } else {
+            if (!this.right) {
+              this.right = node;
+            } else {
+              this.right.add(node);
+            }
+          }
+    }
+  }
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+B.add(A);
+B.add(D);
+B.add(C);;
+console.log(B,D);
+
+
+class PersonTreeNode {
+    constructor(person) {
+      this.value = person.name;
+      this.person = person;
+      this.left = null;
+      this.right = null;
+    }
+  
+    add(node) {
+      // implemented as in previous challenge
+    }
+  
+    findPerson(name) {
+      // Implement me!
+    }
+  }
+
+
+module.exports = { LinkedList, BinaryTreeNode, PersonTreeNode };
